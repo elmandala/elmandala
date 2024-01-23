8fef2535dd8c796b78363d88883bb72afa544d83
elmandala <157066616+elmandala@users.noreply.github.com>
2024-01-23 21:54:06 +0100

    v5

--- general.js
+++ general.js
@@ -11,3 +11,29 @@ function buscarEnPagina() {
         }
     }
 }
+
+// Función para mostrar/ocultar el menú de hamburguesa en dispositivos móviles
+function toggleMenu() {
+    var menu = document.getElementById("menu-lista");
+    var header = document.querySelector(".header-container");
+    var nav = document.querySelector("nav");
+    var closeButton = document.createElement("div");
+
+    closeButton.classList.add("close-button");
+    closeButton.innerHTML = "&times;";
+    closeButton.onclick = function () {
+        document.body.removeChild(this.parentNode);
+    };
+
+    var expandedHeader = document.createElement("div");
+    expandedHeader.classList.add("expanded-header");
+    expandedHeader.appendChild(header.cloneNode(true));
+    expandedHeader.appendChild(nav.cloneNode(true));
+    expandedHeader.appendChild(closeButton);
+
+    if (menu.style.display === "block") {
+        document.body.removeChild(expandedHeader);
+    } else {
+        document.body.appendChild(expandedHeader);
+    }
+}
